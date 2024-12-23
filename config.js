36b22758de9219f08d4ba8824aa18c9cb1b99fdc
abcuos <1585962327@qq.com>
2024-12-23 17:43:51 +0800

    添加喜卡展示逻辑

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 const CONFIG = {
-	// HTTP_PREFIX: "http://8.156.69.47:8080"
+	// HTTP_PREFIX: "http://8.156.69.47:8080",
+	// WS_PREFIX: 'ws://8.156.69.47:8080'
 	HTTP_PREFIX: 'http://localhost:8080',
 	WS_PREFIX: 'ws://localhost:8080'
 };
